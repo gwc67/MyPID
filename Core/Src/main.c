a1af699bdef8fb5d1e4f25c74108efb8a9f3d99d
gwc <3416218577@qq.com>
2026-04-06 14:40:12 +0800

    09_PID_微分先行

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -124,7 +124,7 @@ int main(void)
     // adc_change = adc_normalized - last_adc_normalized;
 
     tft180_BufferClean();
-    Serial_Printf("%f,%f,%f,%f\r\n", Target,Actual,Out,errorInt);
+    Serial_Printf("%f,%f,%f,%f\r\n", Target,Actual,Out,diffout);
     Menu_Choose();
  
     tft180_Buffer_Updata();
--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -214,18 +214,22 @@ void TIM1_UP_IRQHandler(void)
     if (count >= 40)
     {
       count = 0;
+      actual1 = Actual;
       Actual += Encode_Get(); // speed / 1040 =    (speed的单位 边沿数/40ms) 转每40ms
 
       error1 = error0;
       Target = -(data / 4035.0 * 2000 - 1000);
       error0 = Target - Actual;
+      if (Ki != 0 && -500 < errorInt < 500)
+      {
+        errorInt += error0;
+      }
+      else{
+        errorInt = 0;
+      }
+      diffout =- Kd * (Actual - actual1);
 
-      errorInt += error0;
-      float C = 1 / (1 * fabs(error0) + 1);
-
-      errorInt += C * error0;
-
-      Out = Kp * error0 + Ki * errorInt + Kd * (error0 - error1);
+      Out = Kp * error0 + Ki * errorInt + diffout;
       if (Out > 100)
       {
         Out = 100;
